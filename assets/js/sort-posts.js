2388ab0f67dc3b2df503e6890a9583e075032112
Maggie Gong <meijic@outlook.com>
2020-12-17 22:17:21 +0800

    make the "Sort by" dropdown functional

--- assets/js/sort-posts.js
+++ assets/js/sort-posts.js
@@ -1,4 +1,4 @@
-/* ----- Randomizing an array: -----
+/* -------------------- Randomizing an array: --------------------
 
    This is a helper function that randomizes a given array.
    https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -24,13 +24,15 @@ function shuffle(array) {
   return array;
 }
 
-/* ----- Sorting the posts: -----
+/* -------------------- Sorting the posts: --------------------
 
    The initial post list is sorted by date (latest on top)
    Sorting is done by saving a copy of this list as an array,
    then manipulating the elements in this array.
 
 */
+
+// --- Define useful variables
 
 // The post container
 var postContainer = document.querySelector('.post-list');
@@ -44,6 +46,12 @@ for (var i = 0; i < posts.length; i++) {
   postsArray.push(posts[i]);
 }
 
+// The "sort by" dropdown
+var sortBy = document.querySelector('#sort');
+
+// --- Define a few sorting functions
+
+// 1. Sort by latest on top
 function sortByLatest() {
 
   // Erase the content of the post list
@@ -56,6 +64,7 @@ function sortByLatest() {
 
 }
 
+// 2. Sort by oldest on top
 function sortByOldest() {
 
   // Erase the content of the post list
@@ -68,14 +77,28 @@ function sortByOldest() {
 
 }
 
+// 3. Randomize the list
 function sortByRandom() {
 
   // Erase the content of the post list
   postContainer.innerHTML = "";
 
   // Append each element in the postsArray to the post container
-  shuffle(postsArray).forEach(function(post) {
+  shuffle(postsArray.slice()).forEach(function(post) {
     postContainer.appendChild(post);
   });
 
 }
+
+// --- Add event listener to the dropdown
+
+sortBy.addEventListener('input', function(e) {
+  var selected = e.target.value;
+  if (selected === "latest") {
+    sortByLatest();
+  } else if (selected === "oldest") {
+    sortByOldest();
+  } else if (selected === "random") {
+    sortByRandom();
+  }
+});
