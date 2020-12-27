3a281718c280277db2422dd4d60ce3f8f699bbe0
Maggie Gong <meijic@outlook.com>
2020-12-27 19:34:30 +0800

    improve video controls for home page demo

--- assets/js/video-player.js
+++ assets/js/video-player.js
@@ -1,6 +1,34 @@
+// Play buttons (play button & replay button)
+var playBtns = document.querySelectorAll('.play-btn');
+// The video element
 var video = document.querySelector('video');
+// The overlay to display before the video starts
 var videoOverlay = document.querySelector('.video-overlay');
+// The overlay to display when the video ends
+var videoOverlayAfter = document.querySelector('.video-overlay-after');
 
-video.addEventListener('play', function() {
-  videoOverlay.classList.add('hide');
+// Hide the controls at first
+video.controls = false;
+
+// Behaviours of custom play buttons
+playBtns.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Hide the overlays
+    videoOverlay.classList.add('hide');
+    videoOverlayAfter.classList.add('hide');
+    // Play the video
+    video.play();
+    // Show the default video controls while video is playing
+    video.controls = true;
+  });
+});
+
+// When the video ends:
+video.addEventListener('ended', function() {
+  // Move playhead to the beginning of the video
+  this.currentTime = 0;
+  // Hide the video controls
+  video.controls = false;
+  // Show the overlay with "replay" button
+  videoOverlayAfter.classList.remove('hide');
 });
